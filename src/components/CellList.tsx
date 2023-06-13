d3c330ea89caa60575d0fce8f2d863a760880b39
Andriy Bas <andriy.bas@uptech.team>
2023-06-14 00:05:17 +0300

    add add

--- src/components/CellList.tsx
+++ src/components/CellList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import CellListItem from "./CellListItem";
+import AddCell from "./AddCell";
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells }) => {
@@ -11,8 +12,12 @@ const CellList: React.FC = () => {
   return (
     <div>
       {cells.map((cell) => (
-        <CellListItem cell={cell} key={cell.id} />
+        <React.Fragment key={cell.id}>
+          <AddCell nextCellId={cell.id} />
+          <CellListItem cell={cell} />
+        </React.Fragment>
       ))}
+      <AddCell nextCellId={null} forceVisible={cells.length === 0} />
     </div>
   );
 };
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -30,7 +30,7 @@ export const moveCell = (id: string, direction: Direction): MoveCellAction => {
 };
 
 export const insertCellBefore = (
-  id: string,
+  id: string | null,
   cellType: CellType
 ): InsertCellBeforeAction => {
   return {
