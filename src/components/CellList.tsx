04ee3088df959de5e3a8692297a6b1b62b3926fb
Andriy Bas <andriy.bas@uptech.team>
2023-06-15 11:03:01 +0300

    add styles

--- src/components/CellList.tsx
+++ src/components/CellList.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import CellListItem from "./CellListItem";
 import AddCell from "./AddCell";
+import "./CellList.css";
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells }) => {
@@ -10,7 +11,7 @@ const CellList: React.FC = () => {
   });
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell previousCellId={null} forceVisible={cells.length === 0} />
       {cells.map((cell) => (
         <React.Fragment key={cell.id}>
